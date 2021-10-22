03e0809247ae4f47b47151c12778813f94176028
Lastzlo <z.last.zlo@gmail.com>
2021-10-22 16:48:05 +0300

    create entity Task.ts

---
+++ src/app/entity/Task.ts
@@ -0,0 +1,16 @@
+export class Task {
+  private _id: number;
+  private _name: string;
+
+
+  constructor(id: number, name: string) {
+    this._id = id;
+    this._name = name;
+  }
+
+
+  get name(): string {
+    return this._name;
+  }
+}
+
